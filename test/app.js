24012c4b9039f3d479b78ada0e41c0263bd1138f
Advaith Sanil Kumar <advaithsanilkumar@gmail.com>
2024-11-20 12:58:16 +0530

    db testing

--- test/app.js
+++ test/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const {connectToDb,getDb} = require('./db')
-
+const {ObjectId} = require('mongodb')
 const app = express()
 
 // db connection
@@ -19,14 +19,29 @@ app.get('/books', (req,res)=>{
     let books = []
 
     db.collection('books')
-    .find()     // cursor object that points to objects, need to fetch: toArray, forEach
+    .find()     // cursor object that points to objects, need to fetch them using these two: toArray, forEach
     .sort({ author:1 })
     .forEach(book => books.push(book))
     .then(() => {
-        res.statusMessage(200).json(books)
+        res.status(200).json(books)
     })
     .catch(() => {
 res.status(500).json({error: "Could not fetch documents"})
     })
-    res.json({msg: "welcome to the API"})
+})
+
+app.get('/books/:id', (req, res)=>{
+    if(ObjectId.isValid(req.params.id)){
+        db.collection('books')
+        .findOne({_id: new ObjectId(req.params.id)},{projection: {author: 1}})
+        .then(doc => {
+            res.status(200).json(doc)
+        })
+        .catch(err=>{
+            res.status(500).json({error: 'Could not fetch documents'})
+        })
+    }
+    else{
+        res.status(500).json({error: 'Invalid ID'})
+    }
 })
